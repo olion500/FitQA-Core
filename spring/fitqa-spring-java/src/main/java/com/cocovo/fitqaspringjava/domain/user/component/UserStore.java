7da4014ed01db481de6226466c12a0c8741c74fc
olion500 <olion500@daum.net>
2022-04-08 13:53:34 +0900

    Remove: Remove old user domain
    Remove user data from all architecture. It should be recreated.

--- spring/fitqa-spring-java/src/main/java/com/cocovo/fitqaspringjava/domain/user/component/UserStore.java
+++
@@ -1,9 +0,0 @@
-package com.cocovo.fitqaspringjava.domain.user.component;
-
-import com.cocovo.fitqaspringjava.domain.user.entity.User;
-import com.cocovo.fitqaspringjava.domain.user.entity.UserSpec;
-
-public interface UserStore {
-  User store(User initUser);
-  UserSpec store(UserSpec initUserSpec);
-}
--- spring/fitqa-spring-java/src/main/java/com/cocovo/fitqaspringjava/domain/user/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.cocovo.fitqaspringjava.domain.user.service;
-
-import com.cocovo.fitqaspringjava.domain.user.UserCommand;
-
-public interface UserService {
-  String registerUser(UserCommand.RegisterUser registerUser);
-}
