7a47fd050358cc8cac2bafeb2f59b42712e62836
olion500 <olion500@daum.net>
2022-04-30 15:07:50 +0900

    Feat: Receive message from interface layer

---
+++ spring/fitqa-spring-java/src/main/java/com/cocovo/fitqaspringjava/interfaces/message/VideoSqsMessageListener.java
@@ -0,0 +1,18 @@
+package com.cocovo.fitqaspringjava.interfaces.message;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
+import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class VideoSqsMessageListener {
+
+    @SqsListener(value = "fitqa-video-complete", deletionPolicy = SqsMessageDeletionPolicy.NEVER)
+    public void readMessage(VideoCompleteMessage message) {
+        log.info(message.getKey());
+    }
+}
