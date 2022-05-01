9998bc4ce644a81f6ed8f350e862231b858b0c6d
olion500 <olion500@daum.net>
2022-05-01 23:19:01 +0900

    Bugfix: video waiting status saves with string

--- spring/fitqa-spring-java/src/main/java/com/cocovo/fitqaspringjava/domain/video/entity/VideoFeedbackWaiting.java
+++ spring/fitqa-spring-java/src/main/java/com/cocovo/fitqaspringjava/domain/video/entity/VideoFeedbackWaiting.java
@@ -22,6 +22,8 @@ public class VideoFeedbackWaiting extends BaseEntity {
 
     private String feedbackToken;
     private String videoKey;
+
+    @Enumerated(EnumType.STRING)
     private Status status;
 
     @Getter
